dcc70e78c25e59c66d2c38874527f02cd7957115
Kshitij-786 <varshneykshitij769@gmail.com>
2026-08-18 18:19:41 +0530

    1920 done

---
+++ src/Build_Array_from_Permutation_1920.java
@@ -0,0 +1,11 @@
+public class Build_Array_from_Permutation_1920 {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int ans[] = new int[nums.length];
+            for(int i = 0;i<ans.length;i++){
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
